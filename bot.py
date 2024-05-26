af40f1adcf563579773c667cabe324dfe1660b10
Sofus Jejlskov Brandt <sofusjejlskovbrandt@gmail.com>
2024-05-26 18:02:11 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,58 @@
+# bot.py
+import os
+import discord
+from discord import app_commands
+
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv()
+
+# Load Guild ID
+GUILD_ID = os.getenv("GUILD_ID")
+
+# Load intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Create client
+client = discord.Client(intents=intents)
+
+# Create command tree
+tree = app_commands.CommandTree(client)
+
+
+# Commands
+@tree.command(
+    name="hello",
+    description="Social convention of acknowledgement",
+    guild=discord.Object(id=GUILD_ID)  # Remove this eventually. It just has to sit for a while when you do.
+)
+async def hello_command(interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+
+# Events
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
+    print(f'{client.user} has connected to Discord!')
+
+
+# Run
+# Taken from line 28-42 of https://replit.com/@replit/Python-Discord-Bot#main.py
+try:
+    token = os.getenv("DISCORD_TOKEN") or ""
+    if token == "":
+        raise Exception("Please add your token to the Secrets pane.")
+    client.run(token)
+except discord.HTTPException as e:
+    if e.status == 429:
+        print(
+            "The Discord servers denied the connection for making too many requests"
+        )
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
+    else:
+        raise e
