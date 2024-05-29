067abcf83128b420c35c4de84ea40e84b78d747d
Sofus Jejlskov Brandt <sofusjejlskovbrandt@gmail.com>
2024-05-30 00:40:20 +0200

    fix(users): correct usage of double quotes
    Correct usage from double quotes to single quotes in referring to JSON keys.

--- users.py
+++ users.py
@@ -4,16 +4,16 @@ import json
 def steam_id_from_discord_username(discord_username: str):
     users = load_users()
     for user in users:
-        if user["discord_username"] == discord_username:
-            return user["steam_id"]
+        if user['discord_username'] == discord_username:
+            return user['steam_id']
     return None
 
 
 def steam_id_from_discord_id(discord_id: str):
     users = load_users()
     for user in users:
-        if user["discord_id"] == discord_id:
-            return user["steam_id"]
+        if user['discord_id'] == discord_id:
+            return user['steam_id']
     return None
 
 
